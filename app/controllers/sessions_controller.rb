10188ea6397994f5d2a0c23d92afba1e2c8928e8
amritakc <amritakc@gmail.com>
2016-08-11 02:54:37 -0700

    login by name

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,15 @@
 class SessionsController < ApplicationController
 
-
 	def create
+		@user = User.find_by(name: params[:alias])
+		# render :text => @user.name
+
+		if @user && @user.authenticate(params[:password])
+			session[:user_id] = @user.id
+			redirect_to '/profile'
+		else
+			render :text => "did not find alias"
+		end
 	end
 
 
